63acbe2be4e28615bbc1fa687beac4c95ac7c0ef
jeffrose20 <jeffrose20@gmail.com>
2023-05-05 13:12:32 -0400

    Cleaning up import statements

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-import gsheet
-import open_meteo
+from gsheet import gsheet
+from open_meteo import OpenMeteo
 
-gs = gsheet.gsheet(workbook_name="acryl_data")
+# open google sheet
+gs = gsheet(workbook_name="acryl_data")
 
+# loop through input values (lat and long)
 for i, tup in enumerate(gs.inputs):
     gs_row = i+2
     lat = tup[0]
     long = tup[1]
 
-    om = open_meteo.OpenMeteo(lat, long)
+    # pull values from weather API
+    om = OpenMeteo(lat, long)
     gs.add_output_values(row=gs_row, outputs=om.output)
